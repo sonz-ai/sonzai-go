d7fe30a13d63f5142714cb00028ccb6be2aef39c
bryan wahyu <bryanwahyukp95@gmail.com>
2026-03-17 16:24:01 +0700

    feat: add ToolDefinitions field to ChatOptions

--- types.go
+++ types.go
@@ -89,6 +89,13 @@ type AgentToolCapabilities struct {
 }
 
 // ChatOptions configures a chat request.
+// ToolDefinition describes a tool that the agent can invoke during chat.
+type ToolDefinition struct {
+	Name        string      `json:"name"`
+	Description string      `json:"description,omitempty"`
+	Parameters  interface{} `json:"parameters,omitempty"`
+}
+
 type ChatOptions struct {
 	Messages             []ChatMessage         `json:"messages"`
 	UserID               string                `json:"user_id,omitempty"`
@@ -105,6 +112,7 @@ type ChatOptions struct {
 	InteractionRole      string                `json:"interaction_role,omitempty"`
 	Timezone             string                `json:"timezone,omitempty"`
 	ToolCapabilities     *AgentToolCapabilities `json:"tool_capabilities,omitempty"`
+	ToolDefinitions      []ToolDefinition       `json:"tool_definitions,omitempty"`
 }
 
 // ---------------------------------------------------------------------------
